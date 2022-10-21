142a828e06abb12ac0bdf5e5770546836073cd36
Justin Kilburn <ThatMoonMan@gmail.com>
2022-10-21 16:13:11 -0400

    make cat_controller

--- nintey-nine-cats/app/controllers/cats_controller.rb
+++ nintey-nine-cats/app/controllers/cats_controller.rb
@@ -7,23 +7,35 @@ class CatsController < ApplicationController
     end
 
     def show
+        @cat = Cat.find_by(id: params[:id])
 
+        render :show
     end
 
     def new
-
+        render :new
     end
 
     def create
-
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat.id)
+        else
+            render :new
+        end
     end
 
     def edit
-        
+        render :edit
     end
 
     def update
-
+        @cat = Cat.find_by(id: params[:id])
+        if @cat.update
+            redirect_to cat_url(@cat.id)
+        else
+            render :edit
+        end
     end
 
     
